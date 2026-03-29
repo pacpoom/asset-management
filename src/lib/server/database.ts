1bdc83203a2cfbf5549b38ebeb3ab0d0251290f6
pacpoom <pacpoom.a@anji-nyk.com>
2026-03-29 18:19:44 +0700

    db

--- src/lib/server/database.ts
+++ src/lib/server/database.ts
@@ -1,6 +1,7 @@
 import mysql from 'mysql2/promise';
 import { env } from '$env/dynamic/private';
 
+// 1. Main DB Pool
 export const pool = mysql.createPool({
 	host: env.DB_HOST,
 	user: env.DB_USER,
@@ -14,12 +15,25 @@ export const pool = mysql.createPool({
 	keepAliveInitialDelay: 0
 });
 
+// 2. VDC DB Pool
 export const vdcPool = mysql.createPool({
 	host: env.VDC_HOST || '192.168.111.52',
 	port: Number(env.VDC_PORT) || 3308,
 	user: env.VDC_USER || 'root',
 	password: env.VDC_PASSWORD || 'Anji@12345',
 	database: env.VDC_DATABASE || 'vdc_db',
+	waitForConnections: true,
+	connectionLimit: 5,
+	queueLimit: 0
+});
+
+// 3. CYMS DB Pool (เพิ่มเข้ามาใหม่)
+export const cymspool = mysql.createPool({
+	host: env.CYMS_HOST || '192.168.111.52',
+	port: Number(env.CYMS_PORT) || 3307,
+	user: env.CYMS_USER || 'root',
+	password: env.CYMS_PASSWORD || 'Anji@12345',
+	database: env.CYMS_DATABASE || 'anji_db',
 	waitForConnections: true,
 	connectionLimit: 5,
 	queueLimit: 0
